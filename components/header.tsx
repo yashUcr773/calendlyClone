05686ce78eed9aeb1b969f4c92279cec05474ef6
yashUcr773 <yagga004@ucr.edu>
2024-04-04 19:44:09 -0700

    setup auth with kinde

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,12 @@
+"use client"
 import Image from "next/image";
 import { Button } from "./ui/button";
-
+import { RegisterLink, LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 export default function Header() {
+
+    const { isAuthenticated } = useKindeBrowserClient()
+
     return (
         <div>
             <div className="flex flex-row items-center justify-between p-4 shadow-md">
@@ -13,8 +18,20 @@ export default function Header() {
                     <li className="hover:text-primary transition-all duration-300 cursor-pointer">About us</li>
                 </ul>
                 <div className="flex flex-row gap-x-5">
-                    <Button variant={'ghost'}>Login</Button>
-                    <Button>Get Started</Button>
+                    {!isAuthenticated && (
+                        <>
+                            <LoginLink>
+                                <Button variant={'ghost'}>Login</Button>
+                            </LoginLink>
+                            <RegisterLink>
+                                <Button>Get Started</Button>
+                            </RegisterLink>
+                        </>
+                    )}
+
+                    {isAuthenticated && (
+                        <LogoutLink><Button>Logout</Button></LogoutLink>
+                    )}
                 </div>
             </div>
         </div>
---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+export default async function middleware(req: NextRequest) {
+
+    const { isAuthenticated } = getKindeServerSession()
+
+    if (!await isAuthenticated()) {
+        return NextResponse.redirect(new URL('/api/auth/login?post_login_redirect_url=/dashboard', req.url))
+    }
+
+}
+
+export const config = {
+    matcher: ["/dashboard/:path*"]
+};
